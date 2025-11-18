c422ce9f7d0cdca0dd3b92942369e9e9c301d692
kosovic-creator <drasko.kosovic@icloud.com>
2025-11-18 09:28:46 +0100

    gospava

--- apps/client/pages/api/auth/[...nextauth].ts
+++ apps/client/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
 import NextAuth from "next-auth";
-import { authOptions } from '../../../../../packages/auth/dist/auth/nextauth.config.js';
+import { authOptions } from 'auth/dist/auth/nextauth.config.js';
 
 export default NextAuth(authOptions);
